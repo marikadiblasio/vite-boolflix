b87cd518c2653fd8b5c81448a50d46e4055ac3b3
marikadiblasio <marbla.albram@gmail.com>
2023-06-21 12:35:41 +0200

    fixed baseurl

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,6 @@
 import { reactive } from "vue";
 export const store = reactive({
-    baseUrl: 'https:api.themoviedb.org/3/',
+    baseUrl: 'https://api.themoviedb.org/3/',  
     search: {
         api_key: 'f4f73f1cccb8955e7c99926b717eec36',
         query: ''
